6d27b1041b005b602fa010f9eb5e4e0bab3ff2da
Alberto Valverde <alberto@toscat.net>
2025-06-17 16:12:15 +0200

    ready for the vibes

---
+++ agent-vm/nix/hix.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+rec {
+  name = "agent-vm";
+  compiler-nix-name = "ghc910"; # Version of GHC to use
+  index-state = "2025-06-16T20:56:04Z";
+  checkMaterialization = false;
+  # TODO: At some point...
+  #materialized = ./materialized;
+
+  shell.withHoogle = false;
+  # Tools to include in the development shell
+  shell.tools.cabal = "latest";
+  shell.tools.hlint = "latest";
+  shell.tools.ormolu = "latest";
+  # shell.tools.haskell-language-server = "latest";
+  # Non-Haskell tools to include in the development shell
+  shell.nativeBuildInputs =
+    with pkgs.buildPackages;
+    [
+      # hoogle # This one has our local packages!
+      gh # GitHub CLI for PR automation
+    ];
+}
