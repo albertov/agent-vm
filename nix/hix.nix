8eaf1f2822c0cfd15ff369b7b897736adbe30e3f
Alberto Valverde <alberto@toscat.net>
2025-06-23 16:37:38 +0200

    add timeout to cabal_test

--- nix/hix.nix
+++ nix/hix.nix
@@ -5,16 +5,16 @@ rec {
   checkMaterialization = false;
   materialized = ./materialized;
 
-  shell.withHoogle = false;
   # Tools to include in the development shell
   shell.tools.cabal = "latest";
   shell.tools.hlint = "latest";
   shell.tools.ormolu = "latest";
   shell.tools.ghcid = "latest";
   shell.tools.haskell-language-server = "latest";
+  shell.withHoogle = true;
   # Non-Haskell tools to include in the development shell
   shell.nativeBuildInputs = with pkgs.buildPackages; [
-    hoogle # This one has our local packages!
+    # hoogle # This one has our local packages!
     gh # GitHub CLI for PR automation
   ];
 }
